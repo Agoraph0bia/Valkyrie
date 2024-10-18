5acc423aeed261b0030b3a98e5778f0a2f9d1b05
Agoraph0bia <jhoward132@gmail.com>
2024-10-17 21:02:48 -0700

    Working on displaying test data with tanstack table

--- app/components/FolderContentsTable.tsx
+++ app/components/FolderContentsTable.tsx
@@ -1,149 +1,137 @@
+import {
+  createColumnHelper,
+  createTable,
+  getCoreRowModel,
+  RowModel,
+  Table,
+  TableState,
+  Updater,
+  useReactTable,
+} from '@tanstack/react-table';
 import { useRouter } from 'next/navigation';
-import Image from 'next/image';
-import folderimg from '../assets/folder.png';
-import { useEffect, useState } from 'react';
-import { dbOpen } from '../lib/db';
-
-import type {
-	GetServerSideProps,
-	InferGetServerSidePropsType,
-	GetServerSidePropsContext,
-} from 'next';
-
-type Contents = {
-	ID: number;
-	Name: string;
-	Status: number;
-	LastRunTime: number;
-	NextRunTime: number;
-	IsActive: number;
-};
-
-export const getServerSideProps: GetServerSideProps<{
-	contents: Contents;
-}> = async (context) => {
-	const folderid = context.params?.folderid;
-
-	const db = await dbOpen();
-
-	const result = db
-		.prepare(
-			`SELECT f.folderid AS ID,
-				f.[Name],
-				NULL AS Status,
-				NULL AS LastRunTime,
-				NULL AS NextRunTime,
-				f.IsActive
-			FROM folders f
-			WHERE f.parentid = ${folderid}
-				
-			UNION
-
-			SELECT m.monitorid AS ID,
-				m.[Name],
-				t.[Name],
-				m.LastRunTime,
-				m.NextRunTime,
-				m.isactive
-			FROM monitors m
-			JOIN statustypes t
-				ON m.statusid = t.statusid
-			WHERE m.folderid = ${folderid}`
-		)
-		.all();
-	db.close();
-
-	return {
-		props: {
-			result,
-		},
-	};
-};
+import { useMemo } from 'react';
 
 const ContentsTable = async (params: any) => {
-	const router = useRouter();
+  type Item = {
+    ID: number;
+    Name: string;
+    Status: number;
+    LastRunTime: number;
+    NextRunTime: number;
+    IsActive: number;
+  };
 
-	const itemsElements: any = params.items?.map((i: any) => {
-		const detailRow = (
-			<tr>
-				<td colSpan={6} className='h-12 bg-white'></td>
-			</tr>
-		);
+  const columnHelper = createColumnHelper<Item>();
 
-		return (
-			<tr
-				key={i.folderid ?? i.name}
-				onClick={() => {}}
-				onDoubleClick={() =>
-					i.folderid ? router.push(`/folders/${i.folderid}`) : null
-				}
-				className='cursor-default whitespace-nowrap border-b border-b-neutral-600 text-gray-300 hover:bg-zinc-800 focus:bg-blue-900'
-			>
-				<td className='p-3'>
-					<div className='flex flex-col'>
-						{/* {i.folderid ? (
-                            <Image src={folderimg} alt="Logo" />
-                        ) : null} */}
-						<p className='inline-block text-sm font-bold'>{i.name}</p>
-					</div>
-				</td>
-				<td className='text-center'>
-					{i.data?.Status ? (
-						<p className='inline-block text-sm'>{i.data?.Output ?? ''}</p>
-					) : null}
-				</td>
-				<td className='text-center'>
-					{i.data?.Status ? (
-						<p className='inline-block text-sm px-3 py-0.5 rounded-full text-white font-bold ${} bg-sky-600'>
-							{i.data?.Status ?? ''}
-						</p>
-					) : null}
-				</td>
-				<td className='text-center'>
-					{i.data?.Status ? (
-						<p className='inline-block text-sm'>{i.data?.LastRunTime ?? ''}</p>
-					) : null}
-				</td>
-				<td className='text-center'>
-					{i.data?.Status ? (
-						<p className='inline-block text-sm'>{i.data?.Nextruntime ?? ''}</p>
-					) : null}
-				</td>
-				<td className='text-center'>
-					<p className='inline-block text-sm'>Gear</p>
-				</td>
-			</tr>
-		);
-	});
+  const fallback = [];
+  const columns = useMemo(
+    () => [
+      columnHelper.accessor('ID', {
+        header: 'ID',
+      }),
+      columnHelper.accessor('ID', {
+        header: 'Name',
+      }),
+      columnHelper.accessor('ID', {
+        header: 'Status',
+      }),
+      columnHelper.accessor('ID', {
+        header: 'LastRunTime',
+      }),
+      columnHelper.accessor('ID', {
+        header: 'NextRunTime',
+      }),
+    ],
+    []
+  );
 
-	const table = (
-		<table id='contentsTable' className='w-full mt-8 table-fixed'>
-			<thead className='select-none cursor-pointer whitespace-nowrap border-b border-b-neutral-600 font-bold leading-none antialiased text-sm font-sans text-gray-300'>
-				<tr>
-					<th className='text-left p-3 whitespace-nowrap'>
-						<p className='block'>Name</p>
-					</th>
-					<th>
-						<p className='block text-center'>Output</p>
-					</th>
-					<th>
-						<p className=''>Status</p>
-					</th>
-					<th>
-						<p className='block text-center'>Last Run Time</p>
-					</th>
-					<th>
-						<p className='block text-center'>Next Run Time</p>
-					</th>
-					<th>
-						<p className='block text-center'>Action</p>
-					</th>
-				</tr>
-			</thead>
-			<tbody>{itemsElements}</tbody>
-		</table>
-	);
-	return table;
+  const table = useReactTable({
+    columns,
+    data: [],
+    getCoreRowModel: getCoreRowModel(),
+  });
+
+  const detailRow = (
+    <tr>
+      <td colSpan={6} className="h-12 bg-white"></td>
+    </tr>
+  );
+
+  return (
+    //   <tr
+    //     key={}
+    //     onClick={() => {}}
+    //     // onDoubleClick={() =>
+    //     //   i.folderid ? router.push(`/folders/${i.folderid}`) : null
+    //     // }
+    //     className="cursor-default whitespace-nowrap border-b border-b-neutral-600 text-gray-300 hover:bg-zinc-800 focus:bg-blue-900"
+    //   >
+    //     <td className="p-3">
+    //       <div className="flex flex-col">
+    //         {/* {i.folderid ? (
+    //                         <Image src={folderimg} alt="Logo" />
+    //                     ) : null} */}
+    //         <p className="inline-block text-sm font-bold">{i.name}</p>
+    //       </div>
+    //     </td>
+    //     <td className="text-center">
+    //       {i.data?.Status ? (
+    //         <p className="inline-block text-sm">{i.data?.Output ?? ''}</p>
+    //       ) : null}
+    //     </td>
+    //     <td className="text-center">
+    //       {i.data?.Status ? (
+    //         <p className="inline-block text-sm px-3 py-0.5 rounded-full text-white font-bold ${} bg-sky-600">
+    //           {i.data?.Status ?? ''}
+    //         </p>
+    //       ) : null}
+    //     </td>
+    //     <td className="text-center">
+    //       {i.data?.Status ? (
+    //         <p className="inline-block text-sm">{i.data?.LastRunTime ?? ''}</p>
+    //       ) : null}
+    //     </td>
+    //     <td className="text-center">
+    //       {i.data?.Status ? (
+    //         <p className="inline-block text-sm">{i.data?.Nextruntime ?? ''}</p>
+    //       ) : null}
+    //     </td>
+    //     <td className="text-center">
+    //       <p className="inline-block text-sm">Gear</p>
+    //     </td>
+    //   </tr>
+    table
+  );
 };
+
+//   const table = (
+//     <table id="contentsTable" className="w-full mt-8 table-fixed">
+//       <thead className="select-none cursor-pointer whitespace-nowrap border-b border-b-neutral-600 font-bold leading-none antialiased text-sm font-sans text-gray-300">
+//         <tr>
+//           <th className="text-left p-3 whitespace-nowrap">
+//             <p className="block">Name</p>
+//           </th>
+//           <th>
+//             <p className="block text-center">Output</p>
+//           </th>
+//           <th>
+//             <p className="">Status</p>
+//           </th>
+//           <th>
+//             <p className="block text-center">Last Run Time</p>
+//           </th>
+//           <th>
+//             <p className="block text-center">Next Run Time</p>
+//           </th>
+//           <th>
+//             <p className="block text-center">Action</p>
+//           </th>
+//         </tr>
+//       </thead>
+//       <tbody>{itemsElements}</tbody>
+//     </table>
+//   );
+//   return table;
+// };
 
 export default ContentsTable;
--- app/folders/[...folderid]/page.tsx
+++ app/folders/[...folderid]/page.tsx
@@ -1,28 +1,44 @@
-'use client';
-
 import ContentsTable from '../../components/FolderContentsTable';
-import Header from '../../components/Header';
-import SearchBar from '../../components/SearchBar';
-import Image from 'next/image';
-import listViewIcon from './assets/listview.svg';
-import gridViewIcon from './assets/gridview.svg';
 import Breadcrumbs from '../../components/Breadcrumbs';
 
-export default function Page({ items }: any) {
+import { dbOpen } from '@/app/lib/db';
+
+export default async function Page({ params }: { params: { slug: number } }) {
+  const db = await dbOpen();
+
+  const result = db
+    .prepare(
+      `SELECT ROWNUM() AS Key,
+        f.folderid AS ID,
+				f.[Name],
+				NULL AS Status,
+				NULL AS LastRunTime,
+				NULL AS NextRunTime,
+				f.IsActive
+			FROM folders f
+			WHERE f.parentid = ${params.slug}
+				
+			UNION
+
+			SELECT ROWNUM(),
+        m.monitorid,
+				m.[Name],
+				t.[Name],
+				m.LastRunTime,
+				m.NextRunTime,
+				m.isactive
+			FROM monitors m
+			JOIN statustypes t
+				ON m.statusid = t.statusid
+			WHERE m.folderid = ${params.slug}`
+    )
+    .all();
+  db.close();
+
   return (
     <div className="flex flex-col items-center">
-      {/* <header className="select-none text-4xl my-6">Home</header>
-      <SearchBar />
-      <div className="flex flex-row items-center  select-none border-2 rounded-full border-solid border-white mt-5">
-        <button className="transition ease-in-out w-10 p-2 border-r-2 focus:bg-blue-900 border-r-white rounded-l-full">
-          <Image priority src={listViewIcon} alt="x" />
-        </button>
-        <button className="transition ease-in-out w-10 p-3 focus:bg-blue-900 rounded-r-full">
-          <Image priority src={gridViewIcon} alt="x" />
-        </button>
-      </div> */}
       <Breadcrumbs />
-      <ContentsTable items={items} />
+      <ContentsTable items={result} />
     </div>
   );
 }
