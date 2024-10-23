aff51d76cf1b59158a8a9c37469e3ff66887806c
Agoraph0bia <jhoward132@gmail.com>
2024-10-22 22:30:43 -0700

    Got monitors lua script working

--- app/api/servers/[server]/monitors/route.ts
+++ app/api/servers/[server]/monitors/route.ts
@@ -1,75 +1,75 @@
 import { Queue, Worker } from 'bullmq';
 import { connect } from '../../../../lib/db';
+import { NextResponse } from 'next/server';
 
 export const GET = async (
-	request: Request,
-	{ params }: { params: { server: string } }
+  request: Request,
+  { params }: { params: { server: string } }
 ) => {
-	const redis = await connect();
-	const queue = new Queue(params.server);
+  const redis = await connect();
+  const queue = new Queue(params.server);
 
-	redis.defineCommand('getMonitors', {
-		numberOfKeys: 0,
-		readOnly: true,
-		lua: `local monitorNames = redis.call("ZRANGE", "bull:${params.server}:repeat", 0, -1, "WITHSCORES")
+  redis.defineCommand('getMonitors', {
+    numberOfKeys: 0,
+    readOnly: true,
+    lua: `local monitorNames = redis.call("ZRANGE", "bull:${params.server}:repeat", 0, -1, "WITHSCORES")
 
-			  local result = {}
-			  for i,v in pairs(monitorNames) do
-			    if(monitorNames[i + 1] ~= nil ) then
-					local data = redis.call("HGETALL", "bull:${params.server}:repeat:" .. monitorNames[i] .. ":" .. monitorNames[i + 1])
-					table.insert(result, data)
-				end
-			  end
-			  return result`.replace(/\n/g, ' '),
-	});
+		  local result = {}
+		  for i,v in pairs(monitorNames) do
+		    if(monitorNames[i + 1] ~= nil ) then
+			  local data = redis.call("HGETALL", "bull:${params.server}:repeat:" .. monitorNames[i] .. ":" .. monitorNames[i + 1])
+			  table.insert(result, data)
+		    end
+		  end
+		  return result`.replace(/\n/g, ' '),
+  });
 
-	// table.insert(result, {monitorNames[k], monitorNames[k + 1]})
+  const result = await redis.getMonitors();
 
-	// lua: `local monitorNames = redis.call("ZRANGE", "bull:${params.server}:repeat", 0, -1, "WITHSCORES")
+  const monitors = result
+    .filter((m: string[]) => m.length > 0)
+    .map((m: string[]) => {
+      return m.reduce(
+        (current: any, item: string, i: number, monitors: string[]) => {
+          if (i % 2) current[monitors[i - 1]] = item;
+          return current;
+        },
+        {}
+      );
+    }) as {}[];
 
-	// local result = {}
-	// for i,v in ipairs(monitorNames) do
-	//   local data = redis.call("HGETALL", "bull:${params.server}:repeat:" .. v[1] .. ":" .. v[2])
-	//   result[i] = data
-	// end
-	// return result`.replace(/\n/g, ' '),
-
-	const monitors = await redis.getMonitors();
-
-	// const jobData = await redis.hgetall(`bull:${params.server}:repeat:*:`);
-
-	return Response.json(monitors);
+  return NextResponse.json(monitors);
 };
 
 export const PUT = async (
-	request: Request,
-	{ params }: { params: { server: string } }
+  request: Request,
+  { params }: { params: { server: string } }
 ) => {
-	const connection = await connect();
-	const queue = new Queue(params.server);
+  const connection = await connect();
+  const queue = new Queue(params.server);
 
-	const res = await request.json();
+  const res = await request.json();
 
-	const monitor = await queue.upsertJobScheduler(
-		res.monitorName,
-		{
-			pattern: res.pattern,
-			immediately: res.startDate ? true : false,
-			startDate: res.startDate ? new Date(res.startDate) : undefined,
-			endDate: res.endDate ? new Date(res.endDate) : undefined,
-		},
-		{
-			name: res.monitorName,
-			data: res.data,
-			opts: {
-				attempts: res.retries,
-				backoff: {
-					type: 'fixed',
-					delay: res.retryDelay,
-				},
-			},
-		}
-	);
+  const monitor = await queue.upsertJobScheduler(
+    res.monitorName,
+    {
+      pattern: res.pattern,
+      immediately: res.startDate ? false : true,
+      startDate: res.startDate ? new Date(res.startDate) : undefined,
+      endDate: res.endDate ? new Date(res.endDate) : undefined,
+    },
+    {
+      name: res.monitorName,
+      data: res.data,
+      opts: {
+        attempts: res.retries,
+        backoff: {
+          type: 'fixed',
+          delay: res.retryDelay,
+        },
+      },
+    }
+  );
 
-	return Response.json(monitor.toJSON());
+  return Response.json(monitor.toJSON());
 };
--- app/lib/db.ts
+++ app/lib/db.ts
@@ -1,6 +1,10 @@
 import SQLite3, { Database } from 'better-sqlite3';
 import IORedis from 'ioredis';
 
+interface Connection extends IORedis {
+  getMonitors(): any;
+}
+
 export const connect = async () => {
-  return new IORedis();
+  return new IORedis() as Connection;
 };
