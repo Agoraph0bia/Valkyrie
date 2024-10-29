5b25c4cc6d3c9b9f372397dd16666a95c0770b64
Agoraph0bia <jhoward132@gmail.com>
2024-10-28 21:52:58 -0700

    Starting conditions

--- app/classes/worker.ts
+++ app/classes/worker.ts
@@ -2,30 +2,21 @@ import { Worker, Job, FlowProducer, JobNode } from 'bullmq';
 import { Action } from './valkyrie';
 
 const monitorWorker = new Worker('monitor', async (job: Job) => {
-	const flow = new FlowProducer();
+  const flow = new FlowProducer();
 
-	flow
-		.addBulk(
-			job.data.actions.map((a: Action) => {
-				return {
-					name: a.name,
-					queueName: 'actions',
-					opts: {
-						parent: { id: job.id as string, queue: 'monitors' },
-					},
-				};
-			})
-		)
-		.then(() => {});
+  flow
+    .addBulk(
+      job.data.actions.reduce((previousAction: Action, a: Action) => {
+        return {
+          name: a.name,
+          queueName: 'action',
+          opts: {
+            parent: { id: a.parentid as string, queue: 'monitor' },
+          },
+        };
+      })
+    )
+    .then(() => {});
 });
 
-const actionWorker = new Worker('action', async (job: Job) => {
-	// Optionally report some progress
-	await job.updateProgress(42);
-
-	// Optionally sending an object as progress
-	await job.updateProgress({ foo: 'bar' });
-
-	// Do something with job
-	return 'some value';
-});
+const actionWorker = new Worker('action', async (job: Job) => {});
